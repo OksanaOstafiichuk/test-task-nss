74c88fc0b633134e8e17d35cc54399e51b5c6a6a
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-11-07 12:22:44 +0200

    creates search form

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { SearchForm } from "./SearchForm/SearchForm";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Search articles</h1>
+      <SearchForm />
+    </div>
+  );
+};
